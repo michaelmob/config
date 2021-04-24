57f25438f734b5b30eaa4ee0f5d5c07091a4fffc
michaelmob <4530010+michaelmob@users.noreply.github.com>
2021-04-23 23:34:18 -0400

    fetch-lazygit script

---
+++ .scripts/fetch-lazygit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -ex
+cd /tmp
+wget 'https://github.com/jesseduffield/lazygit/releases/download/v0.28.1/lazygit_0.28.1_Linux_x86_64.tar.gz'
+tar xvf lazygit_0.28.1_Linux_x86_64.tar.gz
+cp lazygit ~/.bin/lazygit
